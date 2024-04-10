af8305d073ae6d51cb6159afb978a5cd205241eb
elfaldia <diego.17rojas2002@gmail.com>
2024-04-09 21:53:46 -0400

    pj listo (falta movimiento diagonal)

---
+++ Main character/Scripts/Camera2D.gd
@@ -0,0 +1,8 @@
+extends Camera2D
+
+func _ready():
+	top_level = true
+
+func _process(delta):
+	global_position.x = get_parent().global_position.x
+	global_position.y = get_parent().global_position.y
--- Main character/Scripts/main_character.gd
+++ Main character/Scripts/main_character.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
 
-@export var speed: int = 35
+@export var speed = 100
 @onready var animations = $AnimationPlayer
 
 func handleInput():
-	var moveDirection = Input.get_vector("ui_left","ui_right", "ui_up", "ui_down")
-	velocity = moveDirection*speed
+	var moveDirection = Vector2.ZERO
+	if Input.is_action_pressed("ui_left"):
+		moveDirection.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		moveDirection.x += 1
+	if Input.is_action_pressed("ui_up"):
+		moveDirection.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		moveDirection.y += 1
+	velocity = moveDirection.normalized() * speed
 
 func updateAnimation():
 	if velocity.length() == 0:
