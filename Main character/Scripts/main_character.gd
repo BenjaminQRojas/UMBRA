b2b13682ec1dec07e98372b60d8c53a177ebc998
Diego Martínez <diego.martinez01@alumnos.ucn.cl>
2024-04-12 17:36:50 -0400

    se ajusta linterna a personaje principal

--- Main character/Scripts/main_character.gd
+++ Main character/Scripts/main_character.gd
@@ -51,4 +51,10 @@ func _physics_process(delta):
 	handleInput()
 	move_and_slide()
 	updateAnimation()
+	look_at_mouse()
+	
+
+func look_at_mouse():
+	var mouse_pos = get_global_mouse_position()
+	get_node("Pivote").look_at(mouse_pos)
 	
