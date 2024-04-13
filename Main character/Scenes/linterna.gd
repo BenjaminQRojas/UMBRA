587c363a2d85fb4f247d7ccf00751fcf72f473d6
Daniel Durán <daninsoi924@gmail.com>
2024-04-13 18:20:24 -0400

    detalles minimos

---
+++ Main character/Scenes/linterna.gd
@@ -0,0 +1,12 @@
+extends PointLight2D
+
+
+func _ready():
+	self.enabled = false
+	
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_text_backspace"):
+		if self.enabled == true:
+			self.enabled = false
+		else: 
+			self.enabled = true
