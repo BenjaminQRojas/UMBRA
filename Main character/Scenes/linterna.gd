00c88e8661ea1f988af992d51f8322fa14f8f8ae
Daniel Durán <daninsoi924@gmail.com>
2024-04-13 22:10:16 -0400

    otros detalles minimos

--- Main character/Scenes/linterna.gd
+++ Main character/Scenes/linterna.gd
@@ -2,11 +2,11 @@ extends PointLight2D
 
 
 func _ready():
-	self.enabled = false
+	self.enabled = true
 	
 func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_text_backspace"):
-		if self.enabled == true:
-			self.enabled = false
-		else: 
+		if self.enabled == false:
 			self.enabled = true
+		else: 
+			self.enabled = false
