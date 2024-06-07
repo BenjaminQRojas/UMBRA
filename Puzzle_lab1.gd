9efbed8e615ef526f487cc3feca28e0f1a63c89e
Daniel Durán <daninsoi924@gmail.com>
2024-06-06 21:30:14 -0400

    puzzle funcional

--- Puzzle_lab1.gd
+++ Puzzle_lab1.gd
@@ -1,12 +1,13 @@
 extends Control
 
 var acerto = false
-
+		
 
 func _on_button_pressed():
 	acerto = true
-	visible = not get_tree().paused
-	get_tree().paused = not get_tree().paused
+	Global.transtion_scene = true
+	change_scene("laboratorio")
+	
 
 func _on_button_2_pressed():
 	pass # Replace with function body.
@@ -18,3 +19,12 @@ func _on_button_3_pressed():
 
 func _on_button_4_pressed():
 	pass # Replace with function body.
+
+func change_scene(lugar):
+	if Global.transtion_scene == true:
+		if Global.current_scene == "puzzle":
+			if lugar == "laboratorio":
+				Global.finish_changescenes(lugar)
+				Global.acerto = true
+				get_tree().change_scene_to_file("res://Laboratorio/laboratorio.tscn")
+			
