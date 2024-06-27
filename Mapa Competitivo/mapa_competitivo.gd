b92156f7faaa84ebc495d60620dd5f526ee03730
Daniel Durán <daninsoi924@gmail.com>
2024-06-27 18:48:31 -0400

    solicitudes listas

--- Mapa Competitivo/mapa_competitivo.gd
+++ Mapa Competitivo/mapa_competitivo.gd
@@ -1,10 +1,34 @@
 extends Node
 
+var url = "https://ucn-game-server.martux.cl/scores"
+
 @onready var heartsContainer = $CanvasLayer/heartsContainer
 @onready var player = $TileMap/OnlineCharcter
+@onready var http_request = $HTTPRequest
 
 func _ready():
 	heartsContainer.setMaxHearts(player.maxHealth)
 	heartsContainer.updateHearts(player.currentHealth)
 	player.healthChanged.connect(heartsContainer.updateHearts)
+	
+	$HTTPRequest.request_completed.connect(_on_request_completed)
+	
 
+
+func juego_iniciado():
+	var headers = ["Content-Type: application/json"]
+	http_request.request(url, headers, HTTPClient.METHOD_GET)
+
+func juego_terminado(nickname, ptj):
+	var headers = ["Content-Type: application/json"]
+	var body = {"playername": nickname, "score": ptj}
+	#var json_body = to_json(body)
+	http_request.request(url, headers, HTTPClient.METHOD_POST, body)
+	
+func send_request():
+	var headers = ["Content-Type: application/json"]
+	http_request.request(url, headers, HTTPClient.METHOD_GET)
+	
+func _on_request_completed(result, response_code, headers, body):
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	print(json["Godot 4.2.2"])
---
+++ prueba_conexion.gd
@@ -0,0 +1,69 @@
+extends Node
+
+var url = "https://ucn-game-server.martux.cl/scores"
+var api_key: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzVjMzliZS1iNGJlLTRhNDgtYTIwMy03NzhkNDZkNzc1NjEiLCJrZXkiOiJDY1RzRVpWVXlEU2VMZnU4MmFaWHNMTXQzIiwiaWF0IjoxNzE5NDYxNTMzLCJleHAiOjE3NTA5OTc1MzN9.tLk9cTCOg0ENw00sMCW6qNJ9e0Tfvzt__IxLqWSAJtg"
+@onready var http_request = $HTTPRequest
+
+func _ready():
+	http_request.request_completed.connect(Callable(self, "_on_request_completed"))
+	#juego_iniciado()
+	#juego_terminado("DonTito2", 13)
+	#eliminar_datos()
+
+func juego_iniciado():
+	var headers = [
+		"Authorization: Bearer " + api_key
+	]
+	var error = http_request.request(url, headers, HTTPClient.METHOD_GET)
+	if error != OK:
+		print("Error al enviar la solicitud HTTP GET: %s" % str(error))
+	else: 
+		print("Solicitud HTTP GET enviada con éxito")
+	
+func juego_terminado(nickname, ptj):
+	var headers = ["Content-type: application/json", "Authorization: Bearer " + api_key]
+	
+	var json_body = {
+		"playerName": nickname,
+		"score": int(ptj)
+	}
+	
+	var body = JSON.stringify(json_body)
+	
+	var send_data = http_request.request(url, headers, HTTPClient.METHOD_POST, body)
+	
+	if send_data != OK:
+		print("Hay un error al enviar la solicitud HTTP POST: %s" % str(send_data))
+	else:
+		print("Solicitud HTTP POST enviada con éxito")
+
+func eliminar_datos():
+	var headers = [
+		"Authorization: Bearer " + api_key
+	]
+	var eliminados = http_request.request(url, headers, HTTPClient.METHOD_DELETE)
+	if eliminados != OK:
+		print("Error al enviar la solicitud HTTP DELETE: %s" % str(eliminados))
+	else:
+		print("Solicitud HTTP DELETE enviada con éxito")
+
+func _on_request_completed(result, response_code, headers, body):
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	if json.has("data"):
+		print(json["data"])
+	else:
+		print("El JSON devuelto no contiene la clave 'data'")
+
+
+
+
+func _on_solicitar_pressed():
+	juego_iniciado()
+
+
+func _on_agregar_pressed():
+	juego_terminado("DonTito3", 144)
+
+
+func _on_eliminar_pressed():
+	eliminar_datos()
